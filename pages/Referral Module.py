13e2a5a749e96048a6c1826e2917fa1df4de569c
longman86 <ademola.atolagbe@avonhealthcare.com>
2025-04-02 13:07:20 +0100

    Co-authored-by: Ifeoluwa Adeniyi <ifeoluwa.adeniyi@avonhealthcare.com>

--- pages/Referral Module.py
+++ pages/Referral Module.py
@@ -34,30 +34,41 @@ def get_data_from_sql():
         +';PWD='
         + password
         )
+    # conn = pyodbc.connect(
+    #     'DRIVER={ODBC Driver 17 for SQL Server};SERVER='
+    #     +st.secrets['server']
+    #     +';DATABASE='
+    #     +st.secrets['database']
+    #     +';UID='
+    #     +st.secrets['username']
+    #     +';PWD='
+    #     +st.secrets['password']
+    #     )
     provider_referal = pd.read_sql(query,conn)
     conn.close()
     return provider_referal
 #use the function above to retrieve the data and assign to a varaible
 referral_df = get_data_from_sql()
 #convert the description column to upper case
-referral_df['StandardDescription'] = referral_df['StandardDescription'].str.upper()
-#filter for only procedures
-referral_df = referral_df[
-    (referral_df['ServiceCategory'] == 'Procedure') |
-    (referral_df['ServiceCategory'] == 'Service') |
-    (referral_df['ServiceCategory'] == 'Consultation')
-    ]
+referral_df['CPTDescription'] = referral_df['CPTDescription'].str.upper()
+
+# #filter for all service categories except drugs and consumables
+# referral_df = referral_df[
+#     (referral_df['ServiceCategory'] == 'PROCEDURE') |
+#     (referral_df['ServiceCategory'] == 'SERVICE') |
+#     (referral_df['ServiceCategory'] == 'CONSULTATION')
+#     ]
 #create a list of unique services in the dataframe
-unique_service = referral_df['StandardDescription'].unique()
+unique_service = referral_df['CPTDescription'].unique()
 #create a sidebar select box to select required service from the unique list of services above
 selected_service = st.sidebar.selectbox(label='Select Service', options=unique_service)
 #create a list of unique location states where the selected services are available
-unique_loc = referral_df.loc[referral_df['StandardDescription'] == selected_service, 'State'].unique()
+unique_loc = referral_df.loc[referral_df['CPTDescription'] == selected_service, 'State'].unique()
 #create a sidebar selectbox that enables users to select the state
 selected_loc = st.sidebar.selectbox(label='Select Location', options=unique_loc)
 #create a list of unique class of provider offering the selected service in a particular State
 unique_class = referral_df.loc[
-    (referral_df['StandardDescription'] == selected_service) &
+    (referral_df['CPTDescription'] == selected_service) &
     (referral_df['State'] == selected_loc),
     'ProviderClass'
 ].unique()
@@ -68,7 +79,7 @@ if not referral_df.empty:
     #select only columns with the standard tariffs
     sel_cols = ['Level_1', 'Level_2', 'Level_3', 'Level_4', 'Level_5']
     #create a dataframe for only the standard tariffs for the selected service
-    display_df = referral_df[referral_df['StandardDescription'] == selected_service][sel_cols]
+    display_df = referral_df[referral_df['CPTDescription'] == selected_service][sel_cols]
     #set of instructions to be executed if dataframe is not empty
     if not display_df.empty:
         #extract the tariff for each level and assign to a variable
@@ -98,14 +109,14 @@ else:
 
 #filter for only providers offering the selected service in the selected state and provider class
 sel_service_df = referral_df[
-    (referral_df['StandardDescription'] == selected_service) &
+    (referral_df['CPTDescription'] == selected_service) &
     (referral_df['State'] == selected_loc) &
     (referral_df['ProviderClass'] == selected_class)
 ]
 #select only certain columns to be displayed and sort the values by the amount the provider is charging for the service
-sel_service_df = sel_service_df[['ProviderName','ProviderClass','CPTDescription','Amount', 'Address','HMOOfficerName','HMODeskPhoneNo','HMOOfficerEmail']].sort_values(by='Amount',ascending=False).reset_index(drop=True)
+sel_service_df = sel_service_df[['ProviderName','ProviderClass','StandardDescription','Amount', 'Address','HMOOfficerName','HMODeskPhoneNo','HMOOfficerEmail']].sort_values(by='Amount',ascending=False).reset_index(drop=True)
 #rename the description column
-sel_service_df.rename(columns={'CPTDescription':'ProviderDescription'}, inplace=True)
+sel_service_df.rename(columns={'StandardDescription':'ProviderDescription'}, inplace=True)
 #drop duplicate providers
 sel_service_df.drop_duplicates(subset=['ProviderName', 'ProviderDescription'])
 #display a header and the list of providers based on the selected options
